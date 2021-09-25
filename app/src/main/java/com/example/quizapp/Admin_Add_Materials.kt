1b9b31e7fcc88fc226ab83b25557228e98d26869
ktmshyamal <ktmshyamal@gmail.com>
2021-09-25 23:23:25 +0530

    Subject Management files creations

---
+++ app/src/main/java/com/example/quizapp/Admin_Add_Materials.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Admin_Add_Materials : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_add_materials)
+    }
+}
