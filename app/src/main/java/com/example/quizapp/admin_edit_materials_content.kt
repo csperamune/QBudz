09d4c482a0703f6e09ca8c87d817a184ba44591a
ktmshyamal <ktmshyamal@gmail.com>
2021-09-28 18:29:35 +0530

    material design

---
+++ app/src/main/java/com/example/quizapp/admin_edit_materials_content.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class admin_edit_materials_content : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_edit_materials_content)
+    }
+}
