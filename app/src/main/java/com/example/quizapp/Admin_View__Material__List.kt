211a1c549de4c4af0a48c69f3225eaad46f09ae9
ktmshyamal <ktmshyamal@gmail.com>
2021-09-29 23:40:02 +0530

    designing edit

--- app/src/main/java/com/example/quizapp/Admin_View__Material__List.kt
+++ app/src/main/java/com/example/quizapp/Admin_View__Material__List.kt
@@ -3,9 +3,9 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class admin_edit_materials_content : AppCompatActivity() {
+class Admin_View__Material__List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_edit_materials_content)
+        setContentView(R.layout.activity_admin_view_material_list)
     }
 }
--- app/src/main/java/com/example/quizapp/User_View_Material_List.kt
+++ app/src/main/java/com/example/quizapp/User_View_Material_List.kt
@@ -3,9 +3,9 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class admin_view__material__list : AppCompatActivity() {
+class User_View_Material_List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_view_material_list)
+        setContentView(R.layout.activity_user_view_material_list)
     }
 }
