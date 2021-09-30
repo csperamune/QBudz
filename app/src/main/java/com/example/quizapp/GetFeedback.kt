c9a1074db82932c39ee6e6bc0fd5d4e613338f51
Kavindu Hasaranga <kavinduhasaranga54@gmail.com>
2021-10-01 00:03:47 +0530

    Database Connection

--- app/src/main/java/com/example/quizapp/GetFeedback.kt
+++ app/src/main/java/com/example/quizapp/GetFeedback.kt
@@ -1,4 +1,37 @@
 package com.example.quizapp
 
-class GetFeedback {
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_feedbackmessage.*
+
+
+class GetFeedback  : AppCompatActivity() {
+
+    private lateinit var database : FirebaseDatabase
+    private lateinit var referance : DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_getfeedback)
+
+        database = FirebaseDatabase.getInstance();
+        referance = database.getReference("Users")
+        getFeedback()
+
+    }
+
+    private fun getFeedback(){
+        referance.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                Log.e("Cancel",p0.toString())
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Log.e("Cancel",p0.toString())
+            }
+        })
+    }
 }
