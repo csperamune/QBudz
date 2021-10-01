7e893c264d317e4999cd57130c58a59a66d80ea1
Chamith <csperamune@gmail.com>
2021-10-01 11:31:23 +0530

    Add question UI

---
+++ app/src/main/java/com/example/quizapp/Admin_Add_Question.kt
@@ -0,0 +1,11 @@
+package com.example.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Admin_Add_Question : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_add_question)
+    }
+}
