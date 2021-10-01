27371a85d9847f4dcacc2a6efc9e9a40597fcab9
Chamith <csperamune@gmail.com>
2021-10-01 11:42:23 +0530

    Admin view create question UI

--- app/src/main/java/com/example/quizapp/Admin_Create_Question.kt
+++ app/src/main/java/com/example/quizapp/Admin_Create_Question.kt
@@ -3,9 +3,9 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Admin_Add_Question : AppCompatActivity() {
+class Admin_Create_Question : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_add_question)
+        setContentView(R.layout.activity_admin_create_question)
     }
 }
