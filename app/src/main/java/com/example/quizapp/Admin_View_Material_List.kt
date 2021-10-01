ad6020f24a5e48971fc3326d9481c97f4dec1e0b
ktmshyamal <ktmshyamal@gmail.com>
2021-10-01 15:49:16 +0530

    designing edit

--- app/src/main/java/com/example/quizapp/Admin_View_Material_List.kt
+++ app/src/main/java/com/example/quizapp/Admin_View_Material_List.kt
@@ -3,7 +3,7 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Admin_View__Material__List : AppCompatActivity() {
+class Admin_View_Material_List : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_view_material_list)
