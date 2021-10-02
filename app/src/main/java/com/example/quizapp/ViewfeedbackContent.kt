42c30af9d697a7bb9f24997f8e8538dfbbcd5289
Kavindu Hasaranga <kavinduhasaranga54@gmail.com>
2021-10-03 01:04:29 +0530

    ViewFeedback

--- app/src/main/java/com/example/quizapp/ViewfeedbackContent.kt
+++ app/src/main/java/com/example/quizapp/ViewfeedbackContent.kt
@@ -2,10 +2,45 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.UserData
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ViewfeedbackContent : AppCompatActivity() {
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_viewfeedback_content)
+
+        editText.setOnClickListener{(editInfor())}
+    }
+
+    private fun editInfor() {
+        val inflter = LayoutInflater.from(this)
+        val v = inflter.inflate(R.layout.edit_item,null)
+        /**set view*/
+        val userNo = v.findViewById<EditText>(R.id.userNo)
+
+        val addDialog = AlertDialog.Builder(this)
+
+        addDialog.setView(v)
+        addDialog.setPositiveButton("Ok"){
+                dialog,_->
+            val number = userNo.text.toString()
+            Toast.makeText(this,"Adding User Information Success", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+        addDialog.setNegativeButton("Cancel"){
+                dialog,_->
+            dialog.dismiss()
+            Toast.makeText(this,"Cancel", Toast.LENGTH_SHORT).show()
+
+        }
+        addDialog.create()
+        addDialog.show()
     }
 }
