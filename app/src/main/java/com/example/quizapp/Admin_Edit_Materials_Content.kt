d5b83963bc4df9b6c494bce332e7ecf21763bc3a
ktmshyamal <ktmshyamal@gmail.com>
2021-10-02 19:19:25 +0530

    admin materials pack

--- app/src/main/java/com/example/quizapp/Admin_Edit_Materials_Content.kt
+++ app/src/main/java/com/example/quizapp/Admin_Edit_Materials_Content.kt
@@ -1,11 +1,24 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_admin_edit_materials_content.*
 
 class Admin_Edit_Materials_Content : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_edit_materials_content)
+
+        val pdf: Int=0
+
+        btn_upload.setOnClickListener(View.OnClickListener {
+                view: View-> val intent = Intent()
+//            intent.setType("pdf/*")
+            intent.type="application/pdf"
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent,"Select PDF"),pdf)
+        })
     }
 }
--- app/src/main/java/com/example/quizapp/Admin_View_Materials.kt
+++ app/src/main/java/com/example/quizapp/Admin_View_Materials.kt
@@ -1,11 +1,18 @@
 package com.example.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_admin_view_materials.*
 
 class Admin_View_Materials : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_view_materials)
+
+        btn_edit_material.setOnClickListener {
+            val intent = Intent(this,Admin_Edit_Materials_Content::class.java)
+            startActivity(intent)
+        }
     }
 }
