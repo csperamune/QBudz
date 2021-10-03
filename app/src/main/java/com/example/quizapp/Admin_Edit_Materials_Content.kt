8e1121ae95fc1173a9c818656ba2e6830da4cd0c
ktmshyamal <ktmshyamal@gmail.com>
2021-10-03 07:46:36 +0530

    implementation

--- app/src/main/java/com/example/quizapp/Admin_Edit_Materials_Content.kt
+++ app/src/main/java/com/example/quizapp/Admin_Edit_Materials_Content.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_admin_edit_materials_content.*
+import kotlinx.android.synthetic.main.activity_admin_view_material_list.*
 
 class Admin_Edit_Materials_Content : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,12 +14,17 @@ class Admin_Edit_Materials_Content : AppCompatActivity() {
 
         val pdf: Int=0
 
-        btn_upload.setOnClickListener(View.OnClickListener {
+        btn_view_more_list.setOnClickListener(View.OnClickListener {
                 view: View-> val intent = Intent()
 //            intent.setType("pdf/*")
             intent.type="application/pdf"
             intent.setAction(Intent.ACTION_GET_CONTENT)
             startActivityForResult(Intent.createChooser(intent,"Select PDF"),pdf)
         })
+
+        btn_cancle.setOnClickListener {
+            val intent = Intent(this,Admin_View_Materials::class.java)
+            startActivity(intent)
+        }
     }
 }
